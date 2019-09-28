81a36b0b2777955241d5899b47d6987b6d8e3d03
Andrei <mairovichaa@yandex.ru>
2019-09-28 12:46:27 +0300

    Change constructor of UserDoesNotExistException.java

--- src/main/java/com/amairovi/keeper/exception/UserDoesNotExistException.java
+++ src/main/java/com/amairovi/keeper/exception/UserDoesNotExistException.java
@@ -1,7 +1,7 @@
 package com.amairovi.keeper.exception;
 
 public class UserDoesNotExistException extends RuntimeException {
-    public UserDoesNotExistException(String userId) {
-        super("User with id " + userId + " doesn't exist.");
+    public UserDoesNotExistException(String msg) {
+        super(msg);
     }
 }
