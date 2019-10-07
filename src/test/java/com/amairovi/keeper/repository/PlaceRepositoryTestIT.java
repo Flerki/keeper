dedf39a46d273de8df36ee0f53d98b43e64894c4
Andrei <mairovichaa@yandex.ru>
2019-10-07 21:02:05 +0300

    Switch to junit 5

---
+++ src/test/java/com/amairovi/keeper/repository/PlaceRepositoryTestIT.java
@@ -0,0 +1,7 @@
+package com.amairovi.keeper.repository;
+
+import static org.junit.Assert.*;
+
+public class PlaceRepositoryTestIT {
+
+}
--- src/test/java/com/amairovi/keeper/service/UserPlaceServiceTest.java
+++ src/test/java/com/amairovi/keeper/service/UserPlaceServiceTest.java
@@ -5,8 +5,8 @@ import com.amairovi.keeper.model.Place;
 import com.amairovi.keeper.model.User;
 import com.amairovi.keeper.repository.PlaceRepository;
 import com.amairovi.keeper.repository.UserRepository;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -27,7 +27,7 @@ public class UserPlaceServiceTest {
 
     private PlaceRepository placeRepository;
 
-    @Before
+    @BeforeEach
     public void setup() {
         placeRepository = mock(PlaceRepository.class);
         UserRepository userRepository = mock(UserRepository.class);
@@ -107,6 +107,4 @@ public class UserPlaceServiceTest {
                 )
         );
     }
-
-
 }
